ebba63cf4cb889ac3ef6ea933502c1e7d1276318
Sunny <samusevdanil@gmail.com>
2022-05-10 19:00:42 +0900

    it works

--- js/script.js
+++ js/script.js
@@ -1,65 +1,73 @@
+parent.window.onload = function () {
 
+   const tabBtns = document.querySelectorAll('.header__link'),
+      tabsCont = document.querySelectorAll('.tabs__content'),
+      tabsParent = document.querySelector('.header__flex');
 
-const tabBtns = document.querySelectorAll('.header__link'),
-   tabsCont = document.querySelectorAll('.tabs__content'),
-   tabsParent = document.querySelector('.header__flex');
-
-function hideTabContent() {
-   tabsCont.forEach(item => {
-      item.classList.add('hidden');
-   });
-   tabBtns.forEach(item => {
-      item.classList.remove('active');
-   });
-}
-
-function showTabContent(i) {
-   if (i !== 0) {
-      tabBtns[i].classList.add('active');
-   }
-   tabsCont[i].classList.remove('hidden');
-   tabsCont[i].classList.add('visible');
-}
-
-tabsParent.addEventListener('click', (e) => {
-   if (e.target.classList.contains('header__link')) {
-      tabBtns.forEach((item, i) => {
-         if (e.target == item) {
-            hideTabContent();
-            showTabContent(i);
-            window.scrollTo(0, 0);
-         }
+   function hideTabContent() {
+      tabsCont.forEach(item => {
+         item.classList.add('hidden');
+      });
+      tabBtns.forEach(item => {
+         item.classList.remove('active');
       });
    }
-});
 
-hideTabContent();
-showTabContent(2);
+   function showTabContent(i) {
+      if (i !== 0) {
+         tabBtns[i].classList.add('active');
+      }
+      tabsCont[i].classList.remove('hidden');
+      tabsCont[i].classList.add('visible');
+   }
 
-const gramBtns = document.querySelectorAll('.grammarTrigger'),
-   gramCont = document.querySelectorAll('.article'),
-   gramBtnsParent = document.querySelector('.mainMenu');
-
-function hideGramContent() {
-   gramCont.forEach((item) => {
-      item.classList.remove('visible');
-      item.classList.add('hidden');
+   tabsParent.addEventListener('click', (e) => {
+      if (e.target.classList.contains('header__link')) {
+         tabBtns.forEach((item, i) => {
+            if (e.target == item) {
+               hideTabContent();
+               showTabContent(i);
+               window.scrollTo(0, 0);
+            }
+         });
+      }
    });
-}
 
-console.log(gramCont);
+   hideTabContent();
+   showTabContent(0);
 
+   const gramBtns = document.querySelectorAll('.grammarTrigger'),
+      gramCont = document.querySelectorAll('div.article'),
+      gramBtnsParent = document.querySelector('.mainMenu');
 
-
-gramBtnsParent.addEventListener('click', (e) => {
-   if (e.target.classList.contains('grammar__trigger')) {
-      gramBtns.forEach((item, i) => {
-         if (e.target == item) {
-
-         }
+   function hideGramContent() {
+      gramCont.forEach((item) => {
+         item.classList.remove('visible');
+         item.classList.add('hidden');
       });
    }
-});
+
+   function showGramContent(i) {
+      gramCont[i].classList.remove('hidden');
+      gramCont[i].classList.add('visible');
+   }
+
+   hideGramContent();
+
+   console.log(gramBtnsParent);
+
+   gramBtnsParent.addEventListener('click', (e) => {
+      if (e.target.classList.contains('grammarTrigger')) {
+         gramBtns.forEach((item, i) => {
+            if (e.target == item) {
+               hideGramContent();
+               showGramContent(i);
+               window.scrollTo(0, 0);
+            }
+         });
+      }
+   });
 
 
 
+};
